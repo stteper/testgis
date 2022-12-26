69514f8c615bf810043d65a149e9f3fb163538ca
Alex Stepanov <stteper@mail.ru>
2022-12-26 22:33:09 +0300

    Refactor order filters

--- src/components/OrderFilters/OrderFilters.tsx
+++ src/components/OrderFilters/OrderFilters.tsx
@@ -3,75 +3,58 @@ import React from 'react'
 
 import styles from './OrderFilters.module.scss'
 
+interface FilterItem {
+  link: string
+  title: string
+  counter?: number
+  current?: boolean
+}
+
+const filters: FilterItem[] = [
+  {
+    link: '/',
+    title: 'Все',
+    current: true,
+  },
+  {
+    link: '/',
+    title: 'с откликами',
+    counter: 2,
+  },
+  {
+    link: '/',
+    title: 'активные',
+  },
+  {
+    link: '/',
+    title: 'на паузе',
+  },
+  {
+    link: '/',
+    title: 'завершенные',
+  },
+  {
+    link: '/',
+    title: 'других компаний',
+  },
+]
+
 const OrderFilters: React.FC = () => {
   return (
     <nav className={styles.orderFilters}>
       <ul className={styles.filterItems}>
-        <li
-          className={cn({
-            [styles.filterItem]: true,
-            [styles.active]: true,
-          })}
-        >
-          <a href='/' className={styles.filterLink}>
-            Все
-          </a>
-        </li>
-
-        <li
-          className={cn({
-            [styles.filterItem]: true,
-            [styles.active]: false,
-          })}
-        >
-          <a href='/' className={styles.filterLink}>
-            с откликами <span className={styles.filterCounter}>2</span>
-          </a>
-        </li>
-
-        <li
-          className={cn({
-            [styles.filterItem]: true,
-            [styles.active]: false,
-          })}
-        >
-          <a href='/' className={styles.filterLink}>
-            активные
-          </a>
-        </li>
-
-        <li
-          className={cn({
-            [styles.filterItem]: true,
-            [styles.active]: false,
-          })}
-        >
-          <a href='/' className={styles.filterLink}>
-            на паузе
-          </a>
-        </li>
-
-        <li
-          className={cn({
-            [styles.filterItem]: true,
-            [styles.active]: false,
-          })}
-        >
-          <a href='/' className={styles.filterLink}>
-            завершенные
-          </a>
-        </li>
-
-        <li
-          className={cn({
-            [styles.filterItem]: true,
-            [styles.active]: false,
-          })}
-        >
-          <a href='/' className={styles.filterLink}>
-            других компаний
-          </a>
-        </li>
+        {filters.map((filter) => (
+          <li
+            className={cn({
+              [styles.filterItem]: true,
+              [styles.active]: filter.current,
+            })}
+          >
+            <a href={filter.link} className={styles.filterLink}>
+              {filter.title} {filter.counter ? <span className={styles.filterCounter}>2</span> : null}
+            </a>
+          </li>
+        ))}
       </ul>
     </nav>
   )
